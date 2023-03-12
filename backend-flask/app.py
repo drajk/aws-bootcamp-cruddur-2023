8b1c2817f2756950706fc4376986cf732fb44500
Dheeraj Khushalani <dheerajkhs@gmail.com>
2023-03-12 10:21:02 +0000

    feat: cloudwatch logs

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -3,6 +3,8 @@ from flask import request
 from flask import jsonify
 from flask_cors import CORS, cross_origin
 import os
+import watchtower
+import logging
 
 from services.home_activities import *
 from services.notification_activities import *
@@ -14,6 +16,16 @@ from services.message_groups import *
 from services.messages import *
 from services.create_message import *
 from services.show_activity import *
+from time import strftime
+
+# Configuring Logger to Use CloudWatch
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+LOGGER.addHandler(console_handler)
+LOGGER.addHandler(cw_handler)
+LOGGER.info("some message")
 
 # -- HONEYCOMB --
 from opentelemetry import trace
@@ -63,6 +75,12 @@ cors = CORS(
   methods="OPTIONS,GET,HEAD,POST"
 )
 
+@app.after_request
+def after_request(response):
+    timestamp = strftime('[%Y-%b-%d %H:%M]')
+    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+    return response
+
 @app.route("/api/health", methods=['GET'])
 @cross_origin()
 def health():
@@ -108,7 +126,7 @@ def data_create_message():
 @app.route("/api/activities/home", methods=['GET'])
 @cross_origin()
 def data_home():
-  data = HomeActivities.run()
+  data = HomeActivities.run(logger=LOGGER)
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
